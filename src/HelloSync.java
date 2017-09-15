07cf9d08fe6160c7f548720db71ce54010e97388
robuntu <robuntu@localhost>
2017-09-15 14:20:00 -0400

    New HelloSync program

--- src/HelloSync.java
+++ src/HelloSync.java
@@ -3,7 +3,7 @@ public class HelloSync {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello from school.");
+		System.out.println("Hello from school!");
 	}
 
 }
